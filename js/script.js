bf2d0f18dccbb6394b7a0dd24be34d3a8cc91160
kaii1teak <badlyu@icloud.com>
2024-11-25 11:19:06 +0100

    modified:   css/style.css 	modified:   index.html 	modified:   js/script.js     проба адаптации

--- js/script.js
+++ js/script.js
@@ -6,3 +6,12 @@ faqs.forEach(services => {
   })
 })
 
+document.addEventListener('DOMContentLoaded', function() {
+  const burgerMenu = document.querySelector('.burger-menu');
+  const mobileMenu = document.querySelector('.mobile-menu');
+  
+  burgerMenu.addEventListener('click', function() {
+      burgerMenu.classList.toggle('active');
+      mobileMenu.classList.toggle('active');
+  });
+});
